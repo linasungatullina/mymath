434878f3359919a9629436434bd18f3e51d5d871
Ангелина Сунгатуллина <enn.angel@mail.ru>
2023-09-14 13:36:33 +0300

    Math

--- mymath.go
+++ mymath.go
@@ -3,8 +3,17 @@ package mymath
 import "math"
 
 // Sqrt - функция для вычисления квадратного корня из числа
+
 func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
+}
+
+func Ceil(x float64) float64 {
+	return math.Ceil(x)
+}
+
+func Round(x float64) float64 {
+	return math.Round(x)
 }
 
 func Floor(x float64) float64 {
@@ -29,4 +38,100 @@ func Abs(x float64) float64 {
 
 func Yn(n int, x float64) float64 {
 	return math.Yn(n, x)
+}
+
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
+
+func Asinh(x float64) float64 {
+	return math.Asinh(x)
+}
+
+func Atan(x float64) float64 {
+	return math.Atan(x)
+}
+
+func Atanh(x float64) float64 {
+	return math.Atanh(x)
+}
+
+func Atan2(y, x float64) float64 {
+	return math.Atan2(y, x)
+}
+
+func Cos(x float64) float64 {
+	return math.Cos(x)
+}
+
+func Cosh(x float64) float64 {
+	return math.Cosh(x)
+}
+
+func Sin(x float64) float64 {
+	return math.Sin(x)
+}
+
+func Sinh(x float64) float64 {
+	return math.Sinh(x)
+}
+
+func Tan(x float64) float64 {
+	return math.Tan(x)
+}
+
+func Tanh(x float64) float64 {
+	return math.Tanh(x)
+}
+
+func Hypot(x, y float64) float64 {
+	return math.Hypot(x, y)
+}
+
+func Log(x float64) float64 {
+	return math.Log(x)
+}
+
+func Log10(x float64) float64 {
+	return math.Log10(x)
+}
+
+func Log2(x float64) float64 {
+	return math.Log2(x)
+}
+
+func Logb(x float64) float64 {
+	return math.Logb(x)
+}
+
+func Log1p(x float64) float64 {
+	return math.Log1p(x)
+}
+
+func Exp(x float64) float64 {
+	return math.Exp(x)
+}
+
+func Exp2(x float64) float64 {
+	return math.Exp2(x)
+}
+
+func Expm1(x float64) float64 {
+	return math.Expm1(x)
+}
+
+func Mod(x, y float64) float64 {
+	return math.Mod(x, y)
+}
+
+func Modf(x float64) (float64, float64) {
+	return math.Modf(x)
 }
